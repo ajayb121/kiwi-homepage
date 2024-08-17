334f0b04aebabc49eba1aeddabc912da44b08012
ajaybrahmacanopy <ajay.brahma@canopy.rent>
2024-08-17 14:43:32 +0100

    feat: hide menu on click outside

--- src/app/sections/Navbar/MobileNavbar/index.tsx
+++ src/app/sections/Navbar/MobileNavbar/index.tsx
@@ -1,19 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./index.module.css";
-import "./index.module.css";
 
 const MobileNavbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className={styles.mobileWrapper}>
+    <div className={styles.mobileWrapper} ref={menuRef}>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href="/">
